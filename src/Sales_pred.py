fe77a2fc913db260a55ddad290a92d2bb92c167a
Jacob Jaroya <jaroyajo@gmail.com>
2023-06-07 10:28:58 +0200

    commit app changes

---
+++ src/Sales_pred.py
@@ -0,0 +1,4 @@
+import streamlit as st
+# Add title to my app
+st.title("Welcome TCorporation Favorita - Sales Prediction System")
+
--- src/sales_reg.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-# Add title to my app
-st.title("Welcome to Corporation Favorita - Sales Prediction System")
-
